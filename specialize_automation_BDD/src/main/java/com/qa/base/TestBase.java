da37a1a7b8989dc6d712a0acbaaee4abc6c385b3
rama <r4ramanmatrix@gmail.com>
2022-03-12 16:35:19 +0530

    update_1 in BaseClass

--- specialize_automation_BDD/src/main/java/com/qa/base/TestBase.java
+++ specialize_automation_BDD/src/main/java/com/qa/base/TestBase.java
@@ -1,8 +1,14 @@
 package com.qa.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import com.qa.utils.PropertyReader;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 
@@ -11,7 +17,17 @@ public class TestBase {
 
 	public static void initialization(String url) {
 		if (browserName.equalsIgnoreCase("chrome")) {
-
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--start-maximized");
+			driver = new ChromeDriver(options);
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--start-maximized");
+			driver = new FirefoxDriver(options);
+		} else {
+			System.out.println("Invalid Input!!");
 		}
 	}
 
