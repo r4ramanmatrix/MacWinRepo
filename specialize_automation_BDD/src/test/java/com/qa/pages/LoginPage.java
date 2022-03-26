d93ff7b4f2d8ebbab3915fa124bde96b25ddcfef
rama <r4ramanmatrix@gmail.com>
2022-03-26 14:20:10 +0530

    update Loginpage from Mac machine

--- specialize_automation_BDD/src/test/java/com/qa/pages/LoginPage.java
+++ specialize_automation_BDD/src/test/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,7 @@
 package com.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.qa.base.TestBase;
@@ -8,6 +10,22 @@ public class LoginPage extends TestBase {
 
 	public LoginPage() {
 		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//*[contains(text(),'Log in')]")
+	private WebElement loginLink;
+
+	@FindBy(id = "Email")
+	private WebElement emailBox;
+
+	@FindBy(id = "Password")
+	private WebElement passwordBox;
+
+	@FindBy(xpath = "//*[@type='submit' and @value='Log in'] ")
+	private WebElement loginButton;
+
+	public void clickOnLoginLink() {
+		loginLink.click();
 	}
 
 }
--- specialize_automation_BDD/src/test/java/com/qa/stepdefinitions/StepDef.java
+++ specialize_automation_BDD/src/test/java/com/qa/stepdefinitions/StepDef.java
@@ -3,7 +3,6 @@ package com.qa.stepdefinitions;
 import com.qa.base.TestBase;
 
 import cucumber.api.DataTable;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -17,28 +16,21 @@ public class StepDef extends TestBase {
 
 	@When("^user click on the login link$")
 	public void user_click_on_the_login_link() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+
 	}
 
 	@Then("^user should be on the login page$")
 	public void user_should_be_on_the_login_page() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+
 	}
 
 	@Then("^user enters the below credentials and click on login button$")
 	public void user_enters_the_below_credentials_and_click_on_login_button(DataTable arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		// For automatic transformation, change DataTable to one of
-		// List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-		// E,K,V must be a scalar (String, Integer, Date, enum etc)
-		throw new PendingException();
+
 	}
 
 	@Then("^user should be on the homepage$")
 	public void user_should_be_on_the_homepage() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+
 	}
 }
