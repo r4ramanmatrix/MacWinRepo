174d803f8a219c6e5da02ba8bfe42f7161965f4b
Windows S <W4WindowsSystem@gmail.com>
2022-04-24 12:04:40 +0530

    update homepage

---
+++ specialize_automation_BDD/src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,47 @@
+package com.qa.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.base.TestBase;
+import com.qa.utils.InterfaceImplementation;
+
+public class HomePage extends TestBase {
+
+	WebDriver driver;
+
+	InterfaceImplementation interfaceMethods = null;
+
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		interfaceMethods = new InterfaceImplementation();
+	}
+
+	@FindBy(xpath = "(//*[@class='account'])[1]")
+	private WebElement userDetails;
+
+	@FindBy(xpath = "//*[@class='header']//a")
+	private List<WebElement> headerLinks;
+
+	public String homepageTitle() {
+		return driver.getTitle();
+	}
+
+	public boolean isUserNameDisplay() {
+		return userDetails.isDisplayed();
+	}
+
+	public String getUserName() {
+		return interfaceMethods.getTextOfElement(userDetails);
+	}
+
+	public int getNumberOfHeaderLinks() {
+		return headerLinks.size();
+	}
+
+}
