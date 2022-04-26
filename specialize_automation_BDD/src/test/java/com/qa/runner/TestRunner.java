da2ed610c42f48d014bd247616df54509e08d0bb
Windows S <W4WindowsSystem@gmail.com>
2022-04-26 23:04:08 +0530

    update the testrunner file

--- specialize_automation_BDD/src/test/java/com/qa/runner/TestRunner.java
+++ specialize_automation_BDD/src/test/java/com/qa/runner/TestRunner.java
@@ -6,8 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "resources/features" }, glue = { "com.qa.stepdefinitions",
-		"com.qa.utils" }, plugin = {}, monochrome = true, dryRun = false)//, tags = { "@Smoke_Case" })
+@CucumberOptions(features = { "resources/features" }, glue = { "com.qa.stepdefinitions", "com.qa.utils" }, plugin = {
+		"pretty", "html:target/cucumber-reports" }, monochrome = true, dryRun = false, tags = {})
 
 public class TestRunner {
 
