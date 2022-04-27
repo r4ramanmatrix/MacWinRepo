dea4e2848637c4d2b598e07f53c7f3b5a08b9d58
Windows S <W4WindowsSystem@gmail.com>
2022-04-27 21:05:44 +0530

    update utilities class

--- specialize_automation_BDD/src/main/java/com/qa/utils/Utilities.java
+++ specialize_automation_BDD/src/main/java/com/qa/utils/Utilities.java
@@ -1,5 +1,26 @@
 package com.qa.utils;
 
-public class Utilities {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+import com.qa.base.TestBase;
+
+public class Utilities extends TestBase {
+
+	// To scroll till the end of the page
+	public static void scrollDown() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+	}
+
+	public static void highLighterMethod(WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
+	}
+
+	public static void drawBorder(WebElement element) {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].style.border='3px solid red'", element);
+	}
 
 }
